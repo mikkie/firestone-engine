4cec17951e08a0e5eb5d1f1ec2c61e416cd9b66f
benjaminl <benjamin.li@ehealth.com>
2019-11-19 10:20:02 +0800

    add test tushare for proxy and none proxy

--- tests/TestProxyManager.py
+++ tests/TestProxyManager.py
@@ -1,23 +1,31 @@
 import unittest
+import tushare
+import time
 from firestone_engine.ProxyManager import ProxyManager
 
 class TestProxyManager(unittest.TestCase):
     
     
     def setUp(self):
-        self.proxyManager = ProxyManager()
-
-    def test_get_proxy(self):
         proxy_list = []
+        self.proxyManager = ProxyManager()
         self.proxyManager.get_proxy()
         self.assertGreater(self.proxyManager.get_pool_size(), 0)
         i = 0
         while i <= self.proxyManager.get_pool_size() + 1:
+            time.sleep(2)
             proxy = self.proxyManager.get_proxy()
             proxy_list.append(proxy)
             print(proxy)
             i += 1
-        self.assertEqual(proxy_list[0], proxy_list[len(proxy_list) - 1]) 
+        self.assertEqual(proxy_list[0], proxy_list[len(proxy_list) - 1])
+
+
+    def test_use_proxy(self):
+        df = tushare.get_realtime_quotes('600093',proxyManager=self.proxyManager)
+        self.assertEqual(len(df), 1)
+        df = tushare.get_realtime_quotes('600093')
+        self.assertEqual(len(df), 1)
 
 
 if __name__ == "__main__":
