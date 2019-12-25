021383d83ebf435386f56f2e39a3778cc4ac4be6
benjaminl <benjamin.li@ehealth.com>
2019-12-25 09:13:22 +0800

    change e2e time from 120 to 150

--- tests/e2e/trade/run.bat
+++ tests/e2e/trade/run.bat
@@ -4,5 +4,5 @@ mongo 127.0.0.1/firestone-data --eval "db.getCollection('000993-2019-10-30').dro
 mongo 127.0.0.1/firestone-data --eval "db.getCollection('000993-2019-10-30-m').drop(); db.getCollection('sh-2019-10-30-m').drop();"
 start dist\main.exe 000993 sh --date 2019-10-30 --hours %time:~0,2% --minutes * -m -v
 start dist\calculate.exe 5da1800e87b64fb6f4c32503 --date 2019-10-30-m --hours %time:~0,2% --minutes *  -m -t -v -i
-timeout /t 120 /nobreak > nul
+timeout /t 150 /nobreak > nul
 pipenv run python -m unittest tests/e2e/trade/CheckTrade.py
