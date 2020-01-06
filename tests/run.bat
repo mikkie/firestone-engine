3cf22e7e0eea8fb3f12710e0c29d8d2951cfece0
benjaminl <benjamin.li@ehealth.com>
2020-01-06 17:34:58 +0800

    add pick stock by concept strategy

--- tests/run.bat
+++ tests/run.bat
@@ -4,8 +4,8 @@ mongoimport -d firestone-test -c codes "tests\concept\codes.json"
 mongoimport -d firestone-test -c concepts "tests\concept\concepts.json"
 mongoimport -d firestone-test -c hot_concept "tests\concept\hot_concept.json"
 mongo 127.0.0.1/firestone-test "C:/aqua/firestone-engine/tests/concept/new_hot_concept.js"
-mongo 127.0.0.1/firestone-data "c:/aqua/firestone-engine/tests/initData.js"
-mongo 127.0.0.1/firestone-data "c:/aqua/firestone-engine/tests/strategies/ydls.js"
+REM mongo 127.0.0.1/firestone-data "c:/aqua/firestone-engine/tests/initData.js"
+REM mongo 127.0.0.1/firestone-data "c:/aqua/firestone-engine/tests/strategies/ydls.js"
 pipenv run python -m unittest tests/strategies/TestBasic.py
 pipenv run python -m unittest tests/strategies/TestBasicSell.py
 pipenv run python -m unittest tests/strategies/TestYdls.py
