32d2ad7c368fef9073b0485c29d7d5bd48f12a78
benjaminl <benjamin.li@ehealth.com>
2020-01-17 16:06:34 +0800

    prevent index too hot

--- src/firestone_engine/strategies/ConceptPick.py
+++ src/firestone_engine/strategies/ConceptPick.py
@@ -59,7 +59,7 @@ class ConceptPick(object):
                 return
             concepts = []
             for concept in hot_concepts:
-                if(float(concept['index_percent'][:-1]) < float(self.trade['params']['index_percent']) or int(concept['company_count']) > int(self.trade['params']['company_count']) or float(concept['stock_percent'][:-1]) < float(self.trade['params']['stock_percent']) or float(concept['net_buy']) < float(self.trade['params']['net_buy'])):
+                if(float(concept['index_percent'][:-1]) < float(self.trade['params']['index_percent']) or float(concept['index_percent'][:-1]) >= float(self.trade['params']['max_percent']) or int(concept['company_count']) > int(self.trade['params']['company_count']) or float(concept['stock_percent'][:-1]) < float(self.trade['params']['stock_percent']) or float(concept['net_buy']) < float(self.trade['params']['net_buy'])):
                     continue
                 if(self.trade['params']['concepts'] != "" and self.trade['params']['concepts'].find(concept['name']) < 0):
                     continue
@@ -136,7 +136,7 @@ class ConceptPick(object):
             time.sleep(3)
         full_df['percent'] = (full_df['price'].astype(float) - full_df['pre_close'].astype(float)) / full_df['pre_close'].astype(float) * 100
         index_percent = float(concept['index_percent'][:-1])
-        full_df = full_df[(full_df['percent'] > index_percent) & (full_df['percent'] < float(self.trade['params']['max_percent']))]
+        full_df = full_df[(full_df['percent'] > index_percent) & (full_df['percent'] <= float(self.trade['params']['max_percent']))]
         if(len(full_df) == 0):
             ConceptPick._logger.warning(f"no match stocks for concept {concept['name']} after rank")
             return
