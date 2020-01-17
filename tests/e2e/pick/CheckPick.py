36477f70393a0ec970865ce4e0d01d6c634a830e
benjaminl <benjamin.li@ehealth.com>
2020-01-17 10:16:23 +0800

    fix e2e test bug

--- tests/e2e/pick/CheckPick.py
+++ tests/e2e/pick/CheckPick.py
@@ -1,15 +1,19 @@
 from firestone_engine.Mock import Mock
+from pymongo import MongoClient
 import unittest
 
 class CheckPick(unittest.TestCase):
 
     def setUp(self):
+        self.client = MongoClient('127.0.0.1', 27017)
+        self.db = self.client['firestone-test']
         self.mock = Mock('5db7e0a555609bb27252edb7', date='2020-01-06')
         self.mock.init_Config()
         self.mock.load_trade()
         
     def testTradeState(self):
-        self.assertGreaterEqual(self.mock.trade['result'].find('创建监控:'), 0)
+        mocktrades = list(self.db['mocktrades'].find({}))
+        self.assertGreater(len(mocktrades), 1)
 
     def tearDown(self):
         self.mock.close()
